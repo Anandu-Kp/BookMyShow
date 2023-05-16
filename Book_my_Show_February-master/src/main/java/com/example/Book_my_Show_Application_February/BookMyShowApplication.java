11a98849a303479f77c765610e9a01e1eb4d9192
Anandu-Kp <anandukp06@gmail.com>
2023-05-16 23:33:02 +0530

    final

---
+++ Book_my_Show_February-master/src/main/java/com/example/Book_my_Show_Application_February/BookMyShowApplication.java
@@ -0,0 +1,11 @@
+package com.example.Book_my_Show_Application_February;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+}
